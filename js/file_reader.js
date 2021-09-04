ab342393445cce777e779b7b79ef85a76337dfb0
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2021-09-04 13:35:51 -0600

    Website

---
+++ js/file_reader.js
@@ -0,0 +1,7 @@
+// As with JSON, use the Fetch API & ES6
+fetch('https://github.com/daniel2483/PythonProjects/blob/master/Examples/calculate_age.py')
+  .then(response => response.text())
+  .then(data => {
+  	// Do something with your data
+  	console.log(data);
+  });
